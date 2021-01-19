463b38227daa088fca4923b61286a4ebcf6dec7d
ilyaswilian <74714631+ilyaswilian@users.noreply.github.com>
2021-01-19 19:42:24 +0700

    Update donasi.js

--- plugins/donasi.js
+++ plugins/donasi.js
@@ -1,10 +1,10 @@
 let handler = async m => m.reply(`
 ╭─「 Donasi • Pulsa 」
-│ • Indosat Ooredoo [0815-1586-0089]
-│ • Telkomsel: [0813-5730-2007]
-│ • Smartfren: [0882-3543-5804]
+│ • Indosat Ooredoo [-]
+│ • 3: [0895704283153]
+│ • Smartfren: [-]
 │ 「 Chat OWNER 」
-│ > Ingin donasi? Wa.me/6288235435804
+│ > Ingin donasi? Wa.me/62895704283153
 ╰────
 `.trim()) // Tambah sendiri kalo mau
 handler.help = ['donasi']
